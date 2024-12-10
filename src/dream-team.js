891703a09c3cef36e31144d5f7ede32fa1008953
Gena230295 <gena23solov@gmail.com>
2024-12-10 13:47:45 +0300

    feat: slove dream team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,39 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  const arr = [];
+  for (let i = 0; i < members.length; i += 1) {
+    if (typeof members[i] === "string") {
+      arr.push(members[i].trim().toUpperCase());
+    }
+  }
+
+  const sortArr = arr.sort();
+
+  let str = "";
+  for (let i = 0; i < sortArr.length; i += 1) {
+    str += sortArr[i][0];
+  }
+  return str;
 }
 
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
