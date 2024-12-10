4fd0694a138555fe01c5ea4fde01d7adaae6a1fa
Gena230295 <gena23solov@gmail.com>
2024-12-10 13:20:35 +0300

    feat: slove Delete digit and dns-stats tasks

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given some integer, find the maximal number you can obtain
@@ -11,11 +11,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  const arr = String(n).split("");
+  let num = 0;
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < arr[i + 1]) {
+      arr.splice(i, 1);
+      num += 1;
+      break;
+    }
+  }
+  const newArr = num === 1 ? arr : arr.slice(0, arr.length - 1);
+  const newNum = Number(newArr.join(""));
+  return newNum;
 }
 
 module.exports = {
-  deleteDigit
+  deleteDigit,
 };
--- src/dns-stats.js
+++ src/dns-stats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
@@ -22,11 +22,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const arr = [];
+  for (let i = 0; i < domains.length; i++) {
+    arr.push(domains[i].split(".").reverse());
+  }
+
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    for (let s = 0; s < arr[i].length; s++) {
+      newArr.push(arr[i].slice(0, s + 1));
+    }
+  }
+
+  const stillArr = [];
+  for (let i = 0; i < newArr.length; i++) {
+    stillArr.push(`.${newArr[i].join(".")}`);
+  }
+
+  const setArr = [...new Set(stillArr)];
+
+  const obj = {};
+
+  for (let i = 0; i < setArr.length; i++) {
+    let num = 1;
+    for (let s = 0; s < stillArr.length; s++) {
+      if (setArr[i] === stillArr[s]) {
+        obj[setArr[i]] = num;
+        num++;
+      }
+    }
+    num = 1;
+  }
+  return obj;
 }
 
 module.exports = {
-  getDNSStats
+  getDNSStats,
 };
