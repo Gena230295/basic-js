fc81a41b7a6015cbf686db059615d8525b0c4057
Gena230295 <gena23solov@gmail.com>
2024-12-11 10:54:47 +0300

    feat: slove tasks 'mac-adress' and 'get-email-domain'

--- src/mac-address.js
+++ src/mac-address.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * The MAC-48 address is six groups of two hexadecimal digits (0 to 9 or A to F),
@@ -14,10 +14,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(n) {
+  const needvalue = "ABCDEF0123456789";
+  const arr = n.split("-");
+
+  if (arr.length !== 6) {
+    return false;
+  }
+
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i].length !== 2) {
+      return false;
+    }
+    for (let s = 0; s < arr[i].length; s += 1) {
+      if (!needvalue.includes(arr[i][s])) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
 module.exports = {
-  isMAC48Address
+  isMAC48Address,
 };
