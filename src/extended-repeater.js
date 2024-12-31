37bd8484ba3e5e3facace9d6fadcc228b5c56367
Gena230295 <gena23solov@gmail.com>
2024-12-31 14:46:29 +0300

    feat: slove task extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,51 @@ const { NotImplementedError } = require("../extensions/index.js");
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError("Not implemented");
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let additionStr = "";
+  if (options.addition !== undefined) {
+    if (options.additionRepeatTimes !== undefined && options.additionRepeatTimes > 0) {
+      for (let i = 0; i < options.additionRepeatTimes; i++) {
+        if (options.additionSeparator !== undefined) {
+          if (i < options.additionRepeatTimes - 1) {
+            additionStr += `${options.addition}${options.additionSeparator}`;
+          } else {
+            additionStr += `${options.addition}`;
+          }
+        } else {
+          if (i < options.additionRepeatTimes - 1) {
+            additionStr += `${options.addition}|`;
+          } else {
+            additionStr += `${options.addition}`;
+          }
+        }
+      }
+    } else {
+      additionStr += options.addition;
+    }
+  }
+
+  let mainStr = "";
+  if (options.repeatTimes !== undefined && options.repeatTimes > 0) {
+    for (let i = 0; i < options.repeatTimes; i++) {
+      if (options.separator !== undefined) {
+        if (i < options.repeatTimes - 1) {
+          mainStr += `${str}${additionStr}${options.separator}`;
+        } else {
+          mainStr += `${str}${additionStr}`;
+        }
+      } else {
+        if (i < options.repeatTimes - 1) {
+          mainStr += `${str}${additionStr}+`;
+        } else {
+          mainStr += `${str}${additionStr}`;
+        }
+      }
+    }
+  } else {
+    mainStr += `${str}${additionStr}`;
+  }
+  return mainStr;
 }
 
 module.exports = {
